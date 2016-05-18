1b8288f5993b8437fd8f602dc7706ea6321b4fb3
urzp <urzp002@mail.ru>
2016-05-18 19:57:33 +0300

    Try to find out how convert A-Z,a-z (reverted from commit 778f16476533e69d09c42a06da49039bef60daa9)

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,14 +1,4 @@
 puts "Put your text below:"
-#text=gets.chomp
-text = "In this project, we'll build a program that takes a user's input, then builds a hash from that input. Each key in the hash will be a word from the user; each value will be the number of times that word occurs. For example, if our program gets the string \"the rain in Spain falls mainly on the plain\", it will return"
+text=gets.chomp
 puts "Put your Caesar key below:"
-cesar_key=gets.chomp.to_iwords
-
-letters=text.split("")
-
-letters.each do |c| 
-	
-#puts c if c.ord.between?(97,122)
-puts c if c.ord.between?(65,90)
-	
-end
+cesar_key=gets.chomp.to_i
