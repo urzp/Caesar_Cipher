86b6649e3223cb619dc463a3cd3443312dcb8c51
urzp <urzp002@mail.ru>
2016-05-18 20:06:59 +0300

    Take default text for faster testing

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,6 @@
 puts "Put your text below:"
-text=gets.chomp
+#text=gets.chomp
+text = "In this project, we'll build a program that takes a user's input, then builds a hash from that input. Each key in the hash will be a word from the user; each value will be the number of times that word occurs. For example, if our program gets the string \"the rain in Spain falls mainly on the plain\", it will return"
 puts "Put your Caesar key below:"
 cesar_key=gets.chomp.to_i
 
